25320681d1f1bc113c528eaa7d3b0ee4ba242f74
Mudit Choudhary <cmudit60@gmail.com>
2024-07-17 11:03:47 +0530

    Update user model and doctor model

--- src/model/DoctorModel.js
+++ src/model/DoctorModel.js
@@ -1,29 +1,50 @@
 import mongoose from "mongoose";
-import connectToDB from "../db/dbConfig.js";
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
+
+const appointmentSchema = new Schema({
+  appointmentOn: {
+    type: Date,
+    required: true,
+  },
+  appointmentAt: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+});
 const DoctorSchema = new Schema({
-    fullName:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    speciality:{
-        type:String,
-        required:true
-    },
-    disable:{
-        type:Boolean,
-        default:false
-    }
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  speciality: {
+    type: String,
+    required: true,
+  },
+  disable: {
+    type: Boolean,
+    default: false,
+  },
+  appointments: {
+    type: [appointmentSchema],
+    default: [],
+  },
+});
 
-})
-
-export const DoctorModel = mongoose.model("Doctor",DoctorSchema)
+export const DoctorModel = mongoose.model("Doctor", DoctorSchema);
--- src/model/User.Model.js
+++ src/model/User.Model.js
@@ -2,6 +2,34 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
+const appointmentSchema = new Schema({
+  appointmentOn: {
+    type: Date,
+    default: Date.now,
+  },
+  appointmentAt: {
+    type: String,
+    default: () => {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, "0");
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      return `${hours}:${minutes}`;
+    },
+  },
+  doctorName: {
+    type: String,
+    required: true,
+  },
+  doctorSpeciality: {
+    type: String,
+    required: true,
+  },
+  prescription: {
+    type: String,
+    default: null,
+  },
+});
+
 const UserSchema = new Schema({
   fullName: {
     type: String,
@@ -14,6 +42,14 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true,
+  },
+  userType: {
+    type: String,
+    default: "user",
+  },
+  appointments: {
+    type: [appointmentSchema],
+    default: [],
   },
 });
 
